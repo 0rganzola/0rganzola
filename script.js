1d82632e42953986435d2a4e235c5b44333372da
0rganzola <dreweygooey@gmail.com>
2025-07-08 17:59:25 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+let sidenavHTML =
+   '<div class="sidenav">' +
+            '<a href = index.html><logo><img src="images/logo.png" alt="Logo"></logo>' +
+            '<logotext>organzola</logotext></a>' +
+            
+            '<a href="games.html"><navtext>games</navtext></a>'+
+            '<a href="art.html"><navtext>art</navtext></a>'+
+            '<a href="blog.html"><navtext>blog</navtext></a>'+
+            '<a href="about.html"><navtext>about</navtext></a>'+
+            '<socials>'+
+            '<a href="https://bsky.app/profile/organzola.games"><img src="images/bsky.png" alt="Bluesky" style="width:50px;height:50px;"></a>'+
+            '<a href="https://www.linkedin.com/in/drew-shapiro-949604272/"><img src="images/linkedin.png" alt="LinkedIn" style="width:50px;height:50px;"></a>'+
+            '<a href="mailto:drewshap@gmail.com"><img src="images/at.png" alt="E-Mail" style="width:45px;height:45px;padding-left:5px;"></a>'+
+            '</socials>'+
+        '</div>';
+
+if (document.getElementById("nav")) {
+  document.getElementById("nav").innerHTML = sidenavHTML;
+}
